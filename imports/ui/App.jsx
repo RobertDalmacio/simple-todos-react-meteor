e6b2bf5555024170b433f1157d29a5cf336ae05b
Robert Dalmacio <rjohndalmacio@hotmail.com>
2022-10-05 15:19:08 -0400

    Update and Remove

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,6 +7,16 @@ import {TaskForm} from './TaskForm'
 export const App = () => {
   const tasks = useTracker(() => TasksCollection.find({}, {sort: {createdAt: -1}}).fetch())
 
+  const toggledChecked = ({_id, isChecked}) => {
+    TasksCollection.update(_id, {
+      $set: {
+        isChecked: !isChecked
+      }
+    })
+  }
+
+  const deleteTask = ({_id}) => TasksCollection.remove(_id)
+
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
@@ -14,7 +24,12 @@ export const App = () => {
       <TaskForm />
       
       <ul>
-        {tasks.map(task => <Task key={task._id} task={task} />)}
+        {tasks.map(task => <Task 
+          key={task._id} 
+          task={task} 
+          onCheckboxClick={toggledChecked} 
+          onDeleteClick={deleteTask}
+        />)}
       </ul>
     </div>
   )
